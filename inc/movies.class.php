2e06aecbe447923305da5fe32e5414b92e8fa6bc
andresayac <andresayac@gmail.com>
2022-07-22 20:46:05 -0500

    get detail movies by ID

--- inc/movies.class.php
+++ inc/movies.class.php
@@ -23,7 +23,6 @@ class Movies
         );
 
         $this->config = $_config;
-
     }
 
     public function getMovies($page)
@@ -32,13 +31,15 @@ class Movies
             'headers' => $this->headers
         ]);
 
-        if ($response->getStatusCode() != 200) return false;
+        if ($response->getStatusCode() != 200) return [];
 
         $contents = $response->getBody()->getContents();
         $crawler = new Crawler($contents);
 
-        $data = $crawler->filter("#aa-wp > div > div > main > section > ul > li");
+        $filter = (empty($page)) ? '#tab-1 > ul > li' : '#aa-wp > div > div > main > section > ul > li ';
+        $data = $crawler->filter($filter);
 
+        $data_movies = [];
 
         $data->each(function ($node) {
 
@@ -57,22 +58,72 @@ class Movies
             $tmp_array['genres'] = $this->textToArray($node->filter($querySelector['genres'])->text(), ', ', true, 'Género: ');
             $tmp_array['cast'] = $this->textToArray($node->filter($querySelector['cast'])->text(), ', ', true, 'Actores: ');
 
-            array_push($this->data_movies, $tmp_array);
+            array_push($data_movies, $tmp_array);
         });
 
-        return $this->data_movies;
+        return $data_movies;
     }
 
-    public function getMovieDetail($page)
+    public function getMovieDetail($id)
     {
-        $response =  $this->client_cuevana->request('GET', $page, [
+        $response =  $this->client_cuevana->request('GET', $id, [
             'headers' => $this->headers
         ]);
 
-        if ($response->getStatusCode() != 200) return false;
+        if ($response->getStatusCode() != 200) return [];
 
         $contents = $response->getBody()->getContents();
         $crawler = new Crawler($contents);
+
+        $data_movie = [];
+
+        $data_movie['poster'] = str_replace('w185_and_h278', 'w600_and_h900', $crawler->filter('#top-single > div.backdrop > article > div.Image > figure > img')->attr('data-src'));
+        $data_movie['background'] = $crawler->filter('#top-single > div.backdrop > article > div.Image > figure > img')->attr('data-src');
+        $data_movie['title'] = $crawler->filter('#top-single > div.backdrop > article > header > h1')->text();
+        $data_movie['original_title'] = $crawler->filter('#top-single > div.backdrop > article > header > h2')->text();
+        $data_movie['sypnosis'] = $crawler->filter('#top-single > div.backdrop > article > div.Description > p')->text();
+        $data_movie['year'] = $crawler->filter('#top-single > div.backdrop > article > footer > p > span:nth-child(2)')->text();
+        $data_movie['duration'] = $crawler->filter('#top-single > div.backdrop > article > footer > p > span:nth-child(1)')->text();
+        $data_movie['rating'] = $crawler->filter('div.post-ratings > strong:nth-child(7)')->text();
+        $data_movie['director'] = $crawler->filter('#MvTb-Info > ul > li:nth-child(1) > span')->text();
+        $data_movie['genres'] = $crawler->filter('#MvTb-Info > ul > li:nth-child(2) > a')->text();
+        $data_movie['cast'] = $crawler->filter('#MvTb-Info > ul > li.AAIco-adjust.loadactor > a')->text();
+        $data_movie['link_streaming'] = $this->getLinkStreamingMovies($crawler);
+
+
+        return $data_movie;
+    }
+
+
+    public function getLinkStreamingMovies($crawler)
+    {
+        $elements_id_cuevana = [
+            'OptL' => 'Latino',
+            'OptS' => 'Subtitulado',
+            'OptE' => 'Castellano',
+            'OptY' => 'Youtube'
+        ];
+
+
+        $id_languajes_dispo = $crawler->filter('#top-single > div.video.cont > div.TPlayerCn.BgA > div > div > div')->extract(['id']);
+
+        foreach ($id_languajes_dispo as  $value) {
+            $id_tmp = preg_replace('/[0-9]+/', '', $value);
+            if (in_array($id_tmp, ['mdl-help'])) continue;
+            $array_id_sort[$elements_id_cuevana[$id_tmp]][] = $value;
+        }
+
+        $array_data_url_id = [];
+
+        foreach ($array_id_sort as $key => $value) {
+            $array_data_url_id[$key] = [];
+            foreach ($value as  $option_id) {
+                $url_data =  $crawler->filter("#{$option_id}")->children('iframe')->attr('data-src');
+                $array_data_url_id[$key][$option_id] = (isset($url_data)) ? $url_data : '';
+            }
+        }
+
+        return $array_data_url_id;
     }
 
     public function textToArray($string, $delimiter = ',', $replace = false, $replace_search = '', $replace_to = '')
--- movies.example.php
+++ movies.example.php
@@ -12,34 +12,18 @@ require 'inc/movies.class.php';
 
 $cuevana_movies = new Movies($_config);
 
-// Latest movies added
-$cuev_mov_lastest = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_lastest']);
-echo json_encode($cuev_mov_lastest, JSON_PRETTY_PRINT);
 
-// Premiere movies
-$cuev_mov_release = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_release']);
-echo json_encode($cuev_mov_release, JSON_PRETTY_PRINT);
+// cuev_mov_lastest
+// cuev_mov_release
+// cuev_mov_more_views
+// cuev_mov_most_valued
+// cuev_mov_latin
+// cuev_mov_spanish
+// cuev_mov_subtitled
 
-// Most viewed movies
-$cuev_mov_more_views = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_more_views']);
-echo json_encode($cuev_mov_more_views, JSON_PRETTY_PRINT);
-
-
-// Top rated movies
-$cuev_mov_most_valued = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_most_valued']);
-echo json_encode($cuev_mov_most_valued, JSON_PRETTY_PRINT);
+//echo json_encode($cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_lastest']), JSON_PRETTY_PRINT);
 
 
-// Latin dub movies
-$cuev_mov_latin = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_latin']);
-echo json_encode($cuev_mov_latin, JSON_PRETTY_PRINT);
 
-
-// Spanish dub movies
-$cuev_mov_spanish = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_spanish']);
-echo json_encode($cuev_mov_spanish, JSON_PRETTY_PRINT);
-
-
-// Subtitled movies
-$cuev_mov_subtitled = $cuevana_movies->getMovies($_config['path']['movies']['cuev_mov_subtitled']);
-echo json_encode($cuev_mov_subtitled, JSON_PRETTY_PRINT);
+// id movie 57910
+// echo json_encode($cuevana_movies->getMovieDetail('57910'), JSON_PRETTY_PRINT);
